17515c394e8a2e31e627bfc1e4a0b183c253d825
biyu <abiyras6@gmail.com>
2025-11-26 09:42:08 +0700

    edit dashboard

--- app/dashboard/notebook/[notebookId]/page.tsx
+++ app/dashboard/notebook/[notebookId]/page.tsx
@@ -1,27 +1,28 @@
+import NoteCard from "@/components/cards/note-card"
 import { PageWrapper } from "@/components/wrappers/page-wrapper"
-import { getNoteById } from "@/server/notes"
-import RichTextEditor from "@/components/tiptap/rich-text-editor"
-import type { JSONContent } from "@tiptap/react"
+import { getNotebookById } from "@/server/notebook"
+import CreateNoteButton from "@/components/buttons/create-note-button"
 
-type Params = Promise<{noteId:string}>
+type Params = Promise<{notebookId:string}>
 
-export default async function NotePage({params}:{params:Params}) {
+export default async function Page({params}:{params:Params}) {
   // menampilkan email yg terhubung dg session berdasarkan data login
-  const { noteId } = await params
-  const { note } = await getNoteById(noteId)
+  const { notebookId } = await params
+  const { notebook } = await getNotebookById(notebookId)
 
   return (
     <PageWrapper breadcrumbs={[
         {label:"Dashboard",href:"/dashboard"},
-        {label:note?.notebook?.name ?? "Notebook",href:`/dashboard/notebook/${note?.notebook?.id}`},
-        {label:note?.title ?? "Note",href:`/dashboard/note/${noteId}`},
+        {label:notebook?.name ?? "Note",href:`/dashboard/notebook/${notebookId}`},
       ]}
     >
-      <h1>{note?.title}</h1>
-      <RichTextEditor
-        content={note?.content as JSONContent[]}
-        noteId={noteId}
-      />
+      <h1>{notebook?.name}</h1>
+      <CreateNoteButton notebookId={notebookId}/>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {notebook?.notes?.map((note)=>(
+          <NoteCard key={note.id} note={note}/>
+        ))}
+      </div>
 
     </PageWrapper>
   )
